3b920e8e77cbfad444dd5eaadd26621e35de6c79
ale-assis <alexandre7assis@gmail.com>
2025-04-10 15:10:45 -0300

    add

---
+++ formato_prefixo.py
@@ -0,0 +1,15 @@
+import discord
+from discord.ext import commands
+from constants_qa import TOKEN
+
+intents = discord.Intents.default()
+intents.members = True
+
+client = commands.Bot(command_prefix="!", case_insensitive = True, intents=intents)
+
+
+@client.command()
+async def ola(ctx):
+    await ctx.send("AUUUUUU! Iae! Eu sou o Tribunaaaaldo!")
+
+client.run(TOKEN)
---
+++ squarecloud.app
@@ -0,0 +1,5 @@
+DISPLAY_NAME=Tribunaldo
+MAIN=bot.py
+MEMORY=128
+VERSION=recommended
+DESCRIPTION=Oi, eu sou o Tribunaldo e sou um bot especial do servidor Tribunas!
